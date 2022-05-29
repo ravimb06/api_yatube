e44253f33d0b57b23cf1d6216dc1f8b81737eb4d
Ali Bogatyrev <marksheyder06@gmail.com>
2022-05-29 12:53:58 +0300

    final2

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets, status, serializers
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.exceptions import PermissionDenied
 
-from posts.models import Post, Group, Comment
+from posts.models import Post, Group
 from .serializers import PostSerializer, GroupSerializer, CommentSerializer
 
 
